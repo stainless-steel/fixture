fee33dde2bcd27498ce239b96d00ba8d40c59396
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-06 08:33:43 -0400

    Implement find::all and rename with_extension into first_with_extension

--- src/find.rs
+++ src/find.rs
@@ -1,36 +1,34 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
+/// A decision with respect to a file.
+pub enum Verdict {
+    /// Accept and keep searching.
+    AcceptMore,
+    /// Accept and stop searching.
+    AcceptStop,
+    /// Reject and keep searching.
+    RejectMore,
+    /// Reject and stop searching.
+    RejectStop,
+}
+
+/// Look for all files satisfying a condition.
+pub fn all<F>(directory: &Path, condition: F) -> Vec<PathBuf> where F: Fn(&Path) -> bool {
+    some(directory, |path| {
+        if condition(path) { Verdict::AcceptMore } else { Verdict::RejectMore }
+    })
+}
+
 /// Look for the first file satisfying a condition.
 pub fn first<F>(directory: &Path, condition: F) -> Option<PathBuf> where F: Fn(&Path) -> bool {
-    macro_rules! ok(
-        ($result:expr) => (
-            match $result {
-                Ok(ok) => ok,
-                Err(_) => return None,
-            }
-        );
-    );
-
-    if !ok!(fs::metadata(directory)).is_dir() {
-        return None;
-    }
-
-    for entry in ok!(fs::read_dir(&directory)) {
-        let entry = ok!(entry);
-        if ok!(fs::metadata(entry.path())).is_dir() {
-            continue;
-        }
-        if condition(&entry.path()) {
-            return Some(entry.path());
-        }
-    }
-
-    None
+    some(directory, |path| {
+        if condition(path) { Verdict::AcceptStop } else { Verdict::RejectMore }
+    }).pop()
 }
 
 /// Look for the first file with a particular extension.
-pub fn with_extension(directory: &Path, extension: &str) -> Option<PathBuf> {
+pub fn first_with_extension(directory: &Path, extension: &str) -> Option<PathBuf> {
     use std::ascii::AsciiExt;
 
     macro_rules! ok(
@@ -45,4 +43,37 @@ pub fn with_extension(directory: &Path, extension: &str) -> Option<PathBuf> {
     first(directory, |path| -> bool {
         ok!(ok!(path.extension()).to_str()).to_ascii_lowercase() == extension
     })
+}
+
+/// Look for some files satisfying a condition.
+pub fn some<F>(directory: &Path, condition: F) -> Vec<PathBuf> where F: Fn(&Path) -> Verdict {
+    macro_rules! ok(
+        ($result:expr) => (
+            match $result {
+                Ok(ok) => ok,
+                Err(_) => return Vec::new(),
+            }
+        );
+    );
+
+    let mut paths = Vec::new();
+
+    for entry in ok!(fs::read_dir(&directory)) {
+        let entry = ok!(entry);
+        if ok!(fs::metadata(entry.path())).is_dir() {
+            continue;
+        }
+        let path = entry.path();
+        match condition(&path) {
+            Verdict::AcceptMore => paths.push(path),
+            Verdict::AcceptStop => {
+                paths.push(path);
+                break;
+            },
+            Verdict::RejectMore => {},
+            Verdict::RejectStop => break,
+        }
+    }
+
+    paths
 }
