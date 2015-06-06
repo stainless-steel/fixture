a76d7b92bfad424f5458beb3dc1e377d89fab23d
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-06 09:24:09 -0400

    Add exists

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,26 @@
 //! Auxiliary routines for testing.
 
+use std::fs;
+use std::path::Path;
+
 pub mod find;
 pub mod copy;
+
+/// Check if a path exists and is not a directory.
+pub fn exists(path: &Path) -> bool {
+    match fs::metadata(path) {
+        Ok(metadata) => !metadata.is_dir(),
+        _ => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    #[test]
+    fn exists() {
+        assert!(::exists(&Path::new("src/lib.rs")));
+        assert!(!::exists(&Path::new("src")));
+    }
+}
